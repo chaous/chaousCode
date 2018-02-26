20e0993c8cc8b581089e5776ee971a92030b4da1
elvis <e209813@g.unicamp.br>
2018-02-26 14:19:54 -0300

    this is the template to code

---
+++ taskA.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define bug1(x) cout<<#x<<" = "<<x<<endl
+#define bug2(x,y) cout<<#x<<" = "<<x<<" "<<#y<<" = "<<y<<endl
+#define bug3(x,y,z) cout<<#x<<" = "<<x<<" "<<#y<<" = "<<y<<" "<<#z<<" = "<<z<<endl
+#define bug4(x,y,z,m) cout<<#x<<" = "<<x<<" "<<#y<<" = "<<y<<" "<<#z<<" = "<<z<<" "<<#m<<" = "<<m<<endl
+
+using namespace std;
+const int maxN = 200005;
+const int maxK = 505;
+const int mod = (int)1e9 + 7;
+const int inf = (int)1e9;
+
+int main(){
+
+    freopen("in.c","r",stdin);
+
+
+    return 0;
+}
